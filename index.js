0f9a280c2ae5eef1e42ca62c454f3ff9e000bbe5
JJGaspar1727 <johnjosephgaspar@gmail>
2024-03-14 14:44:43 +0800

    3rd commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Nyaaa!!!");
 
-generateNumber(30);
+generateNumber(75);
 
 function generateNumber (max) {
     // addition = +
@@ -14,9 +14,20 @@ function generateNumber (max) {
     //Gt >, Gte > = , Lt <,  Lte < =, Eq =, Neq ! =, And &&, or || 
     if (randomNumber <= 15) {
         console.log('The number is in B.');
-    } else if (randomNumber >= 16 && randomNumber <= 30){
+    }
+    else if (randomNumber >= 16 && randomNumber <= + 30){
         console.log('The number is in I');
-    } else {
+    } 
+    else if (randomNumber >= 31 && randomNumber <= + 45){
+        console.log('The number is in N');
+    } 
+    else if (randomNumber >= 46 && randomNumber <= + 60){
+        console.log('The number is in G');
+    } 
+    else if (randomNumber >= 61 && randomNumber <= + 75){
+        console.log('The number is in O');
+    } 
+    else {
         console.log('The number is invalid!');
     } 
 
