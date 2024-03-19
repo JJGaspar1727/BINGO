ae10842b0a98d45633fcd501299efc8ac11f5a1d
JJGaspar1727 <johnjosephgaspar@gmail.com>
2024-03-19 13:33:45 +0800

    4th commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 console.log("Nyaaa!!!");
 
-generateNumber(75);
+//generateNumber(75);
+
+let isB = false,
+    isI = false,
+    isN = false,
+    isG = false,
+    isO = false;
 
 function generateNumber (max) {
     // addition = +
@@ -9,27 +15,37 @@ function generateNumber (max) {
     // divition = /
     // remainder = %
     let randomNumber = Math.floor(Math.random() * max) + 1;
-
+    
     //if... else if... else
     //Gt >, Gte > = , Lt <,  Lte < =, Eq =, Neq ! =, And &&, or || 
     if (randomNumber <= 15) {
+        isB = true;
         console.log('The number is in B.');
     }
     else if (randomNumber >= 16 && randomNumber <= + 30){
+        isI = true;
         console.log('The number is in I');
     } 
     else if (randomNumber >= 31 && randomNumber <= + 45){
+        isN = true;
         console.log('The number is in N');
     } 
     else if (randomNumber >= 46 && randomNumber <= + 60){
+        isG = true;
         console.log('The number is in G');
     } 
     else if (randomNumber >= 61 && randomNumber <= + 75){
+        isO = true;
         console.log('The number is in O');
     } 
     else {
         console.log('The number is invalid!');
     } 
 
+    if (isB = true && isI == true && isN == true && isG == true && isO == true) {
+        console.log('Wow BINGOOOO!!!');
+    }
     return console.log(randomNumber);
 }
+
+let counter = 10;
